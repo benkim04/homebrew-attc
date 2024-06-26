3e09150c7c8dc68444e8cd7bd737193a89ff375d
benkim04 <benkim@uchicago.edu>
2024-06-26 16:15:54 -0500

    Added the ruby file git branch -M main

---
+++ Formula/attachment-converter.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class AttachmentConverter < Formula
+  desc "Attachment Converter: tool for batch converting attachments in an email mailbox"
+  homepage ""
+  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "9f4bf3fcc71782609d462d6a4671a283aed543c43f83c0e29acfe6fc3db423bc"
+  license "GPL-2.0"
+
+  depends_on "dune" => :build
+  depends_on "ocamlbuild" => :build
+  depends_on "ocaml"
+  
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "make", "home-install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test attachment-converter`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
