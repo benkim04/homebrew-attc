caad4e98ea379fe03f0ce2346c95d50d6779d193
benkim04 <benkim@uchicago.edu>
2024-06-27 11:27:26 -0500

    Changes v3

---
+++ Formula/attc.rb
@@ -0,0 +1,20 @@
+class Attc < Formula
+  desc "Attachment Converter: tool for batch converting attachments in an email mailbox"
+  homepage "https://github.com/benkim04/attachment-converter"
+  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "9f4bf3fcc71782609d462d6a4671a283aed543c43f83c0e29acfe6fc3db423bc"
+  license "GPL-2.0"
+
+  depends_on "dune" => :build
+  depends_on "ocamlbuild" => :build
+  depends_on "ocaml"
+  
+  def install
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    system "make", "home-install"
+  end
+
+  test do
+    system "true"
+  end
+end
