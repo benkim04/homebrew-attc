9590f0557f290d31ff909ba3b5ff132f9fcb51bb
benkim04 <benkim@uchicago.edu>
2024-06-27 15:38:11 -0500

    Fingers crossed

--- Formula/attc.rb
+++ Formula/attc.rb
@@ -1,8 +1,8 @@
 class Attc < Formula
   desc "Attachment Converter: tool for batch converting attachments in an email mailbox"
   homepage "https://github.com/benkim04/attachment-converter"
-  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "e2d58aac840143e353a89bde9c6a87c02cc4c2ed3b654413d6e3eacee5d2bd26"
+  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "6cecd7175c76638abc158e5beb7e2815400a1eb90b41d038dbb6eea00e1bc161"
   license "GPL-2.0"
 
   depends_on "dune" => :build
@@ -13,6 +13,7 @@ class Attc < Formula
   def install
     ENV.deparallelize
     system "make", "brew-install"
+    bin.install "_build/default/main.exe" => "attc"
   end
 
   test do
