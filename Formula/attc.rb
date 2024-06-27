13b148f5a997949575fd9708c0fb51682c240dc2
benkim04 <benkim@uchicago.edu>
2024-06-27 15:57:39 -0500

    Changed version

--- Formula/attc.rb
+++ Formula/attc.rb
@@ -1,8 +1,8 @@
 class Attc < Formula
   desc "Attachment Converter: tool for batch converting attachments in an email mailbox"
   homepage "https://github.com/benkim04/attachment-converter"
-  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/v0.0.7.tar.gz"
-  sha256 "6cecd7175c76638abc158e5beb7e2815400a1eb90b41d038dbb6eea00e1bc161"
+  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/v0.0.8.tar.gz"
+  sha256 "5ca8951f233afa6cdd06044703385ba936575470537720e03f1101f908d019de"
   license "GPL-2.0"
 
   depends_on "dune" => :build
