efbf4500e8f9268273b2461b19d63f2d04ef3b34
benkim04 <benkim@uchicago.edu>
2024-06-27 16:18:49 -0500

    Changed version and install version

--- Formula/attc.rb
+++ Formula/attc.rb
@@ -1,8 +1,8 @@
 class Attc < Formula
   desc "Attachment Converter: tool for batch converting attachments in an email mailbox"
   homepage "https://github.com/benkim04/attachment-converter"
-  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/v0.0.8.tar.gz"
-  sha256 "5ca8951f233afa6cdd06044703385ba936575470537720e03f1101f908d019de"
+  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "99859bdf716c74bb70b91447d8cd3a67687998d09113c7dafa97ecd13278b925"
   license "GPL-2.0"
 
   depends_on "dune" => :build
@@ -12,7 +12,7 @@ class Attc < Formula
 
   def install
     ENV.deparallelize
-    system "make", "home-install"
+    system "make", "brew-install"
     bin.install "_build/default/main.exe" => "attc"
   end
 
