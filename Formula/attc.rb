f116d145c2131372c1d063a5b38750152f182dcb
benkim04 <benkim@uchicago.edu>
2024-07-11 13:57:50 -0500

    CAPITAL LETTER ADDED

--- Formula/attc.rb
+++ Formula/attc.rb
@@ -1,5 +1,5 @@
 class Attc < Formula
-  desc "tool for batch-converting attachments to preservation formats"
+  desc "Tool for batch-converting attachments to preservation formats"
   homepage "https://github.com/benkim04/attachment-converter"
   url "https://github.com/benkim04/attachment-converter/archive/refs/tags/0.1.10.tar.gz"
   sha256 "8aa2b288e956ab23e1e1249b6c43476da3f903fd2920abb4c193f722030fbb6e"
