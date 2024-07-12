7720b252eb48caa341d36abed2a1832e38ed8766
benkim04 <benkim@uchicago.edu>
2024-07-12 12:13:49 -0500

    Reverse reverse

--- Formula/attc.rb
+++ Formula/attc.rb
@@ -1,11 +1,10 @@
 class Attc < Formula
   desc "Tool for batch-converting attachments to preservation formats"
   homepage "https://github.com/benkim04/attachment-converter"
-  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/0.1.11.tar.gz"
-  sha256 "314a860335cd483832221940262c84c2d21fbc49fbf543203050b4dc4672474c"
+  url "https://github.com/benkim04/attachment-converter/archive/refs/tags/0.1.4.tar.gz"
+  sha256 "2f32e6d36ca02fd55c752498ff2af3f638c94268339784fdeccfb32f4f564110"
   license "GPL-2.0"
 
-  depends_on "dune" => :build
   depends_on "opam" => :build
   depends_on "ghostscript"
   depends_on "mercurial"
@@ -17,6 +16,8 @@ class Attc < Formula
     ENV.deparallelize
     system "make", "brew-install"
     bin.install "_build/default/main.exe" => "attc"
+    system "mkdir", "-p", "#{share}/attachment-converter/scripts" 
+    system "cp", "#{prefix}/conversion-scripts/*.sh", "#{share}/attachment-converter/scripts"
   end
 
   def caveats
